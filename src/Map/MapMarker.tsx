51729abbf42bf6314b94fb123e35f18f79e07767
DESKTOP-JTDD7A9\vheh0 <vheh0901@gmail.com>
2023-01-07 00:55:24 +0900

    [Add] : 장소 아이템 클릭시 나타나는 커스텀 팝업 제작

--- src/Map/MapMarker.tsx
+++ src/Map/MapMarker.tsx
@@ -1,6 +1,8 @@
-import { useLayoutEffect, useMemo } from "react";
+import { useEffect, useLayoutEffect, useMemo, useRef } from "react";
 import { useMap } from "../hooks/useMap";
 import { PlaceType } from "./mapTypes";
+import ReactDOM from "react-dom";
+import styled from "@emotion/styled";
 
 interface MapMarkerProps {
   place: PlaceType;
@@ -13,6 +15,8 @@ const MARKER_IMAGE_URL =
 
 const Mapmarker = (props: MapMarkerProps) => {
   const map = useMap();
+  const Container = useRef(document.createElement("div"));
+
   const marker = useMemo(() => {
     const imageSize = new kakao.maps.Size(36, 37); // 마커 이미지의 크기
     const imgOptions = {
@@ -32,7 +36,25 @@ const Mapmarker = (props: MapMarkerProps) => {
       image: markerImage,
     });
 
+    kakao.maps.event.addListener(marker, "click", () => {
+      map.setCenter(props.place.position);
+      map.setLevel(4, {
+        animate: true,
+      });
+      infoWindow.setMap(map);
+    });
+
     return marker;
+  }, []);
+
+  const infoWindow = useMemo(() => {
+    Container.current.style.position = "absolute";
+    Container.current.style.bottom = "40px";
+
+    return new kakao.maps.CustomOverlay({
+      position: props.place.position,
+      content: Container.current,
+    });
   }, []);
 
   useLayoutEffect(() => {
@@ -43,13 +65,58 @@ const Mapmarker = (props: MapMarkerProps) => {
     };
   }, [map]);
 
-  useLayoutEffect(() => {
+  useEffect(() => {
     if (props.showInfo) {
-      console.log(props.place.title);
+      infoWindow.setMap(map);
     }
+
+    return () => {
+      infoWindow.setMap(null);
+    };
   }, [props.showInfo]);
 
-  return <></>;
+  return (
+    <>
+      {Container.current
+        ? ReactDOM.createPortal(
+            <Message
+              onClick={() => {
+                infoWindow.setMap(null);
+              }}
+            >
+              <Name>{props.place.title}</Name>
+              <Address>{props.place.address}</Address>
+            </Message>,
+            Container.current
+          )
+        : null}
+    </>
+  );
 };
+
+const Message = styled.section`
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center
+  width: 180px;
+  min-height: 50px;
+  margin-left: -90px;
+  border-radius: 15px;
+  text-align: center;
+  padding: 10px;
+
+  background-color: rgba(255, 228, 196, 0.9);
+`;
+
+const Name = styled.label`
+  font-weight: 600;
+  padding: 6px 8px;
+`;
+
+const Address = styled.span`
+  font-size: 12px;
+  padding: 0 6px 6px;
+`;
 
 export default Mapmarker;
